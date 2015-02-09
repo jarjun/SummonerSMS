f04ec9dc6b7f11da507d220097cbe8b6c6386ed7
jarjun <arjun.jain@duke.edu>
2015-02-09 17:19:02 -0500

    should work now

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ import requests
 from twilio.rest import TwilioRestClient
 import twilio.twiml
 import os
-
+import time
 
 leagueAPI = os.environ['LEAGUE_API']
 
@@ -20,41 +20,59 @@ def main():
     return str(resp)
 
 def composeMessage(summonerName):
-    summonerNameURL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+summonerName + "/"
+    summonerNameURL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+ summonerName + "/"
 
     query = {
                 "api_key": leagueAPI
               }
-    response = requests.get(summonerNameURL, params = query)
-    idData = response.json()
+    try:
+        response = requests.get(summonerNameURL, params = query)
+        idData = response.json()
+        name = summonerName.split(" ")
+        name = "".join(name)
+        id = idData[name.lower()]["id"]
+    except:
+        return "Summoner not found"
 
-    id = idData[summonerName.lower()]["id"]
 
-    gameURL = "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/" + id + "/"
+    try:
+        gameURL = "https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/" + str(id) + "/"
 
-    response = requests.get(gameURL, params = query)
-    gameData = response.json()
+        response2 = requests.get(gameURL, params = query)
+        gameData = response2.json()
+        participantList = gameData["participants"]
+        infoList = []
+        for x in participantList:
+            infoList.append([x["summonerName"],x["teamId"], x["summonerId"]])
+            if x["summonerId"] == id:
+                team = x["teamId"]
+        opposingTeam =  []
+        for x in infoList:
+            if x[1] != team:
+                opposingTeam.append([x[0],x[2]])
+    except:
+        return "Not in game"
 
-    participantList = gameData["participants"]
-    infoList = []
-    for x in participantList:
-        infoList.append([x["summonerName"],x["teamId"], x["summonerId"]])
-        if x["summonerId"] == id:
-            team = x["teamId"]
-    opposingTeam =  []
-    for x in infoList:
-        if x[1] != team:
-            opposingTeam.append([x[0],x[2]])
 
+
+    d = {}
+    for x in opposingTeam:
+        d[str(x[1])] = x[0]
     finList = []
 
-    for x in opposingTeam:
-        leagueURL = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + x[1] + "/"
-        response = requests.get(leagueURL, params = query)
-        leagueData = response.json()
-        finList.append(x[0] + "-" + leagueData[x[1]]["tier"])
+    try:
+        leagueURL = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + str(opposingTeam[0][1]) + "," + str(opposingTeam[1][1]) + "," + str(opposingTeam[2][1]) + "," + str(opposingTeam[3][1]) + "," + str(opposingTeam[4][1]) + "/"
+        response3 = requests.get(leagueURL, params = query)
+        leagueData = response3.json()
+    except:
+        return "Request Error"
+
+
+    for x in leagueData:
+        finList.append(str(d[x]) + "-" + str(leagueData[x][0]["tier"]))
 
     ret = ""
     for x in finList:
-        ret += (x + "\n")
+        ret += x
+        ret += "\n"
     return ret
