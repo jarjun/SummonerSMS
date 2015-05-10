ab437ffc3f4a319e6f50dc3b7a99c169c4d4d50b
jarjun <arjun.jain@duke.edu>
2015-05-09 22:27:11 -0400

    so close

--- app/views.py
+++ app/views.py
@@ -88,6 +88,7 @@ def composeMessage(summonerName):
         recordingURL = "http://na.op.gg/summoner/ajax/requestRecording.json/gameId=" + str(gameID) + "/"
         response4 = requests.get(recordingURL)
         record = response4.json()
+        ret += str(record)
         if record["success"] == "true":
             ret += "Recording Successful"
         else:
